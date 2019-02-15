6ee2de0dfe030410e72ff61a5a28fa3fa0e20a05
Gleison <gleisonnanet@gmail.com>
2019-02-15 06:12:26 -0200

    first commit

---
+++ Source/BASEJUMP/BASEJUMPGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BASEJUMPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASEJUMP_API ABASEJUMPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
